3ad861632c5b21e6570a22d1dd34f8f2a747e971
wianvanniekerk <wianvanniekerk@protonmail.com>
2024-05-04 10:59:16 +0200

    Dark mode and contact form

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,14 @@
 const pass = process.env.Password;
+const port = process.env.Port;
 
-function sendEmail() {
+function sendEmail(event) {
+  event.preventDefault();
+
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "wianvanniekerk@protonmail.com",
         Password : pass,
+        Port : port,
         To : 'wianvanniekerk@protonmail.com',
         From: 'wianvanniekerk@protonmail.com',
         Body : document.getElementById("email").value,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./build/*.html", "./build/pages/*.html", "./build/js/*.js"],
+  darkMode: 'class',
   theme: {
     extend: {
       screens: {
