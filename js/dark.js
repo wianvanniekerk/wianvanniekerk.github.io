88717d3b85b58f3e143b47821f5ef1eb2eae59cd
wianvanniekerk <wianvanniekerk@users.noreply.github.com>
2024-05-04 09:08:16 +0000

    deploy: 7c98f7d87ec8bf63e7917a9f4245c1d5002076ef

---
+++ js/dark.js
@@ -0,0 +1,16 @@
+const options = {
+    bottom: '64px',
+    right: 'unset',
+    left: '32px',
+    time: '0.5s',
+    mixColor: '#f0fff1',
+    backgroundColor: '#fff',
+    buttonColorDark: '#100f2c',
+    buttonColorLight: '#fff',
+    saveInCookies: true, 
+    label: '🌓',
+    autoMatchOsTheme: true
+  }
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
